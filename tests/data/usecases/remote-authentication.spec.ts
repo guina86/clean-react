3561416f08eb2cd282358b27a468fed645df87ad
Leandro Guina <guina86@gmail.com>
2022-06-06 15:04:23 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct url and body

--- tests/data/usecases/remote-authentication.spec.ts
+++ tests/data/usecases/remote-authentication.spec.ts
@@ -1,20 +1,30 @@
 import { HttpPostClient } from '@data/protocols'
 import { RemoteAuthentication } from '@data/usecases'
+import { AuthenticationParams } from '@domain/usecases'
 import { mock } from 'jest-mock-extended'
 import { faker } from '@faker-js/faker'
 
 describe('RemoteAuthentication', () => {
   let sut: RemoteAuthentication
-  const url = faker.internet.url()
+  let url: string
+  let authParams: AuthenticationParams
   const httpPostClientSpy = mock<HttpPostClient>()
+
+  beforeAll(() => {
+    url = faker.internet.url()
+    authParams = {
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    }
+  })
 
   beforeEach(() => {
     sut = new RemoteAuthentication(url, httpPostClientSpy)
   })
 
-  it('should ', async () => {
-    await sut.auth()
+  it('should call HttpPostClient with correct url and body', async () => {
+    await sut.auth(authParams)
 
-    expect(httpPostClientSpy.post).toHaveBeenCalledWith({ url })
+    expect(httpPostClientSpy.post).toHaveBeenCalledWith({ url, body: authParams })
   })
 })
