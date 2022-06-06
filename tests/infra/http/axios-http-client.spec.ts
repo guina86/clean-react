19637ca6d142a28e0d179f9338b0d9b034f7e9cc
Leandro Guina <guina86@gmail.com>
2022-06-06 19:04:39 -0300

    test: ensure AxiosHttpClient calls axios with correct values

--- tests/infra/http/axios-http-client.spec.ts
+++ tests/infra/http/axios-http-client.spec.ts
@@ -1,24 +1,29 @@
 import { AxiosHttpClient } from '@infra/http'
 import axios from 'axios'
 import { faker } from '@faker-js/faker'
+import { HttpPostParams } from '@data/protocols'
 
 jest.mock('axios')
 
 describe('AxiosHttpClient', () => {
   let sut: AxiosHttpClient
   let url: string
+  let body: any
+  let postParams: HttpPostParams<any>
   const mockedAxios = jest.mocked(axios)
 
   beforeAll(() => {
     url = faker.internet.url()
+    body = { any: 'thing' }
+    postParams = { url, body }
   })
 
   beforeEach(() => {
     sut = new AxiosHttpClient()
   })
 
-  it('should call axios with correct url and verb', async () => {
-    await sut.post({ url })
-    expect(mockedAxios.post).toHaveBeenCalledWith(url)
+  it('should call axios with correct values', async () => {
+    await sut.post(postParams)
+    expect(mockedAxios.post).toHaveBeenCalledWith(url, body)
   })
 })
