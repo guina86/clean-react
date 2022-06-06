55823e6ebc610d6aa328f90f0f8dede12ddddf54
Leandro Guina <guina86@gmail.com>
2022-06-06 13:51:17 -0300

    feat: ensure RemoteAuthentication calls HttpPostClient with correct URL

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}'
   ],
@@ -7,5 +7,10 @@ module.exports = {
   testEnvironment: 'node',
   transform: {
     '^.+\\.tsx?$': '@swc/jest'
+  },
+  moduleNameMapper: {
+    '@tests/(.*)': '<rootDir>/tests/$1',
+    '@domain/(.*)': '<rootDir>/src/domain/$1',
+    '@data/(.*)': '<rootDir>/src/data/$1'
   }
 }
--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,4 +1,4 @@
-import { AccountModel } from 'domain/model/account-model'
+import { AccountModel } from '@domain/model/account-model'
 
 type AuthenticationParams = {
   email: string
