06c02dd808baafa47c6e6bc75b79ba5822d2efef
Leandro Guina <guina86@gmail.com>
2022-06-07 20:49:29 -0300

    refactor: add index files with multiple imports

--- src/presentation/components/login-header/index.tsx
+++ src/presentation/components/login-header/index.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from 'react'
-import Logo from '@presentation/components/logo'
+import { Logo } from '@presentation/components'
 import Styles from './styles.scss'
 
 const LoginHeader: React.FC = () => {
