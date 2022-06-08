a5ee3911ba3889b7209948b1a819666c0ac4ea3a
Leandro Guina <guina86@gmail.com>
2022-06-08 16:08:22 -0300

    test: ensure Login calls validation with correct email

--- tests/presentation/pages/login.spec.tsx
+++ tests/presentation/pages/login.spec.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { cleanup, render, RenderResult, fireEvent } from '@testing-library/react'
 import Login from '@presentation/pages/login'
+import mock from 'jest-mock-extended/lib/Mock'
+import { Validation } from '@presentation/protocols/validation'
 
 describe('Login Component', () => {
   let sut: RenderResult
+  const validationSpy = mock<Validation>()
+
+  beforeAll(() => {
+    validationSpy.validate.mockReturnValue('errorMessage')
+  })
 
   beforeEach(() => {
-    sut = render(<Login />)
+    sut = render(<Login validation={validationSpy} />)
   })
+
+  afterEach(cleanup)
 
   it('should start with initial state', () => {
     const statusWrap = sut.getByTestId('statusWrap')
@@ -20,5 +29,13 @@ describe('Login Component', () => {
     const passwordStatus = sut.getByTestId('password-status')
     expect(passwordStatus.title).toBe('Campo obrigatório')
     expect(passwordStatus.textContent).toBe('🔴')
+  })
+
+  it('should call validation with correct email', () => {
+    const emailInput = sut.getByPlaceholderText('Digite seu e-mail')
+    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    expect(validationSpy.validate).toHaveBeenCalledWith({
+      email: 'any_email'
+    })
   })
 })
