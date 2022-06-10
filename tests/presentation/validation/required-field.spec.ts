c71a63d0196007c054dc4087d183ab5b19253e69
Leandro Guina <guina86@gmail.com>
2022-06-10 14:02:36 -0300

    test: ensure RequiredFieldValidation returns false if field is not empty

--- tests/presentation/validation/required-field.spec.ts
+++ tests/presentation/validation/required-field.spec.ts
@@ -1,5 +1,6 @@
 import { RequiredFieldValidation } from '@presentation/validation'
 import { RequiredFieldError } from '@presentation/validation/errors'
+import { faker } from '@faker-js/faker'
 
 describe('RequiredFieldValidation', () => {
   let sut: RequiredFieldValidation
@@ -12,5 +13,11 @@ describe('RequiredFieldValidation', () => {
     const error = sut.validate('')
 
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  it('should return falsy if field is not empty', () => {
+    const error = sut.validate(faker.random.word())
+
+    expect(error).toBeFalsy()
   })
 })
