06e09cb652e147792a85181cc0f2bb20f280090c
Leandro Guina <guina86@gmail.com>
2022-06-10 10:40:25 -0300

    test: ensure Login add accessToken to localstorage on success

--- tests/presentation/pages/login.spec.tsx
+++ tests/presentation/pages/login.spec.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { render, RenderResult, fireEvent } from '@testing-library/react'
+import { render, RenderResult, fireEvent, waitFor } from '@testing-library/react'
 import Login from '@presentation/pages/login'
 import mock from 'jest-mock-extended/lib/Mock'
 import { Validation } from '@presentation/protocols/validation'
 import { faker } from '@faker-js/faker'
 import { Authentication } from '@domain/usecases'
 import { InvalidCredentialsError } from '@domain/errors'
+import 'jest-localstorage-mock'
 
 describe('Login Component', () => {
   let sut: RenderResult
   const errorMessage = faker.random.words(2)
+  const accessToken = faker.datatype.uuid()
   const validationSpy = mock<Validation>()
   const authenticationSpy = mock<Authentication>()
   const arrangeEmail = (sut: RenderResult, email = faker.internet.email()): string => {
@@ -34,10 +36,11 @@ describe('Login Component', () => {
 
   beforeAll(() => {
     validationSpy.validate.mockReturnValue(errorMessage)
-    authenticationSpy.auth.mockResolvedValue({ accessToken: faker.datatype.uuid() })
+    authenticationSpy.auth.mockResolvedValue({ accessToken })
   })
 
   beforeEach(() => {
+    localStorage.clear()
     jest.clearAllMocks()
     sut = render(<Login validation={validationSpy} authentication={authenticationSpy}/>)
     validationSpy.validate.mockReturnValue('')
@@ -138,5 +141,12 @@ describe('Login Component', () => {
     const mainError = await sut.findByRole('error-message')
     expect(mainError).toHaveTextContent(error.message)
     expect(statusWrap.childElementCount).toBe(1)
+  })
+
+  it('should add accessToken to localstorage on success', async () => {
+    arrangeInputs(sut)
+    actSubmit(sut)
+    await waitFor(async () => sut.getByRole('form'))
+    expect(localStorage.setItem).toHaveBeenCalledWith('accessToken', accessToken)
   })
 })
