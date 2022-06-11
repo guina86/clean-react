44e9722b528a407fc07993b39e033e16dbee985b
Leandro Guina <guina86@gmail.com>
2022-06-10 23:22:47 -0300

    test: ensure ValidantionBuilder returns a list of validations

--- tests/presentation/validation/builder.spec.ts
+++ tests/presentation/validation/builder.spec.ts
@@ -15,4 +15,13 @@ describe('ValidationBuilder', () => {
     const validations = ValidationBuilder.field('any_field').min(5).build()
     expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
   })
+
+  it('should return a list of validations', () => {
+    const validations = ValidationBuilder.field('any_field').required().email().min(5).build()
+    expect(validations).toEqual([
+      new RequiredFieldValidation('any_field'),
+      new EmailValidation('any_field'),
+      new MinLengthValidation('any_field', 5)
+    ])
+  })
 })
