9f47da5f74ed9598bfe83e86b2e748e217bd7748
Leandro Guina <guina86@gmail.com>
2022-06-12 19:00:25 -0300

    test: ensure LocalSaveAccessToken throws if SetStorage throws

--- tests/data/usecases/local-save-access-token.spec.ts
+++ tests/data/usecases/local-save-access-token.spec.ts
@@ -6,15 +6,22 @@ import { mock } from 'jest-mock-extended'
 describe('LocalSaveAccessToken', () => {
   let sut: LocalSaveAccessToken
   const setStorageSpy = mock<SetStorage>()
+  const accessToken = faker.datatype.uuid()
 
   beforeEach(() => {
     sut = new LocalSaveAccessToken(setStorageSpy)
   })
 
   it('should call SetStorage with correct value', async () => {
-    const accessToken = faker.datatype.uuid()
     await sut.save(accessToken)
 
     expect(setStorageSpy.set).toBeCalledWith('accessToken', accessToken)
+  })
+
+  it('should throw if SetStorage throws', async () => {
+    setStorageSpy.set.mockRejectedValueOnce(new Error('any_error'))
+    const promise = sut.save(accessToken)
+
+    await expect(promise).rejects.toThrow(new Error('any_error'))
   })
 })
