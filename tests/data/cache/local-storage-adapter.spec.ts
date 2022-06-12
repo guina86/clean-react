d797a86fe3983e0098ed7c714638752ebd8e6cfa
Leandro Guina <guina86@gmail.com>
2022-06-12 19:01:34 -0300

    test: ensure LocalStorageAdapter calls localStorage with correct values

---
+++ tests/data/cache/local-storage-adapter.spec.ts
@@ -0,0 +1,17 @@
+import { LocalStorageAdapter } from '@infra/cache/local-storage-adapter'
+import 'jest-localstorage-mock'
+
+describe('LocalStorageAdapter', () => {
+  let sut: LocalStorageAdapter
+
+  beforeEach(() => {
+    localStorage.clear()
+    sut = new LocalStorageAdapter()
+  })
+
+  it('should call localStorage with correct values', async () => {
+    await sut.set('any_key', 'any_value')
+
+    expect(localStorage.setItem).toHaveBeenCalledWith('any_key', 'any_value')
+  })
+})
