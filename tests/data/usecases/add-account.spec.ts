02b6c2352ee8f45bb4f19cbd84d8e6fda4155c57
Leandro Guina <guina86@gmail.com>
2022-06-12 23:24:43 -0300

    test: ensure RemoteAddAccount throws EmailInUseError if HttpPostClient returns 403

--- tests/data/usecases/add-account.spec.ts
+++ tests/data/usecases/add-account.spec.ts
@@ -1,5 +1,6 @@
 import { HttpPostClient, HttpStatusCode } from '@data/protocols'
 import { RemoteAddAccount } from '@data/usecases'
+import { EmailInUseError } from '@domain/errors'
 import { AccountModel } from '@domain/model'
 import { AddAccountParams } from '@domain/usecases'
 import { faker } from '@faker-js/faker'
@@ -8,14 +9,14 @@ import { mock } from 'jest-mock-extended'
 describe('RemoteAddAccount', () => {
   let sut: RemoteAddAccount
   let url: string
-  let authParams: AddAccountParams
+  let addParams: AddAccountParams
   let accessToken: string
   const httpPostClientSpy = mock<HttpPostClient<AddAccountParams, AccountModel>>()
 
   beforeAll(() => {
     url = faker.internet.url()
     const password = faker.internet.password()
-    authParams = {
+    addParams = {
       name: faker.name.findName(),
       email: faker.internet.email(),
       password: password,
@@ -30,8 +31,16 @@ describe('RemoteAddAccount', () => {
   })
 
   it('should call HttpPostClient with correct url and body', async () => {
-    await sut.add(authParams)
+    await sut.add(addParams)
 
-    expect(httpPostClientSpy.post).toHaveBeenCalledWith({ url, body: authParams })
+    expect(httpPostClientSpy.post).toHaveBeenCalledWith({ url, body: addParams })
+  })
+
+  it('should throw EmailInUseError if HttpPostClient returns 403', async () => {
+    httpPostClientSpy.post.mockResolvedValueOnce({ statusCode: HttpStatusCode.forbidden })
+
+    const promise = sut.add(addParams)
+
+    await expect(promise).rejects.toThrow(new EmailInUseError())
   })
 })
