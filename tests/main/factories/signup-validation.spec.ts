c60ccf18b20993aedbab10d589edbe0e01cc4dff
Leandro Guina <guina86@gmail.com>
2022-06-14 10:17:44 -0300

    test: add SignUpValidationFactory

---
+++ tests/main/factories/signup-validation.spec.ts
@@ -0,0 +1,16 @@
+import { ValidationFacade } from '@presentation/validation/validators'
+import { makeSignUpValidation } from '@main/factories/validation'
+
+describe('SignUpValidationFactory', () => {
+  it('should make ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation()
+
+    expect(composite).toEqual(new ValidationFacade()
+      .field('name').required().min(3).push()
+      .field('email').required().email().push()
+      .field('password').required().min(5).push()
+      .field('passwordConfirmation').required().min(5).sameAs('password').push()
+      .build()
+    )
+  })
+})
