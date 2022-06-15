f69b5eb45db15867ee282bc1bf799b5ddd00f1e4
Leandro Guina <guina86@gmail.com>
2022-06-15 08:07:12 -0300

    test: ensure login begin with correct initial state

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,5 +1,16 @@
 describe('Login', () => {
-  it('should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+
+  it('should load with correct initial state', () => {
+    cy.getByRole('email-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByRole('password-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.get('button').should('have.attr', 'disabled')
+    cy.getByRole('status-wrap').should('not.have.descendants')
   })
 })
