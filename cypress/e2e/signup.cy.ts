e454e727c62596f7f4cda2791bd40ef3c7e72ac0
Leandro Guina <guina86@gmail.com>
2022-06-17 14:55:55 -0300

    test: ensure SignUp present error state if form is invalid

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -13,4 +13,17 @@ describe('SignUp', () => {
     cy.get('button').should('have.attr', 'disabled')
     cy.getByRole('status-wrap').should('not.have.descendants')
   })
+
+  it('should present error state if form is invalid', () => {
+    cy.getByRole('name-input').type('na')
+    cy.getByRole('email-input').type('invalid_email')
+    cy.getByRole('password-input').type('1234')
+    cy.getByRole('passwordConfirmation-input').type('4321')
+    testInputStatus('name', 'name deve ter pelo menos 3 caracteres')
+    testInputStatus('email', 'email inválido')
+    testInputStatus('password', 'password deve ter pelo menos 5 caracteres')
+    testInputStatus('passwordConfirmation', 'passwordConfirmation deve ter pelo menos 5 caracteres')
+    cy.get('button').should('have.attr', 'disabled')
+    cy.getByRole('status-wrap').should('not.have.descendants')
+  })
 })
