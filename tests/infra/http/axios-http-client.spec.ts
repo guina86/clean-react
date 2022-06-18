1cfedfd5a082151bf0ee6793d745c6f048685be0
Leandro Guina <guina86@gmail.com>
2022-06-18 19:24:35 -0300

    test: ensure AxiosHttpClient calls axios.get with correct values

--- tests/infra/http/axios-http-client.spec.ts
+++ tests/infra/http/axios-http-client.spec.ts
@@ -5,15 +5,13 @@ import { faker } from '@faker-js/faker'
 jest.mock('axios')
 
 describe('AxiosHttpClient', () => {
-  describe('POST', () => {
-    let sut: AxiosHttpClient
-    let url: string
-    let body: any
-    const mockedAxios = axios as jest.Mocked<typeof axios>
+  let sut: AxiosHttpClient
+  const url = faker.internet.url()
+  const body = { any: 'data' }
+  const mockedAxios = axios as jest.Mocked<typeof axios>
 
+  describe('post()', () => {
     beforeAll(() => {
-      url = faker.internet.url()
-      body = { any: 'data' }
       mockedAxios.post.mockResolvedValue({ status: 200, data: 'any_data' })
     })
 
@@ -23,11 +21,13 @@ describe('AxiosHttpClient', () => {
 
     it('should call axios.post with correct values', async () => {
       await sut.post({ url, body })
+
       expect(mockedAxios.post).toHaveBeenCalledWith(url, body)
     })
 
     it('should return correct response on axios.post', async () => {
       const httpResponse = await sut.post({ url, body })
+
       expect(httpResponse).toEqual({
         statusCode: 200,
         body: 'any_data'
@@ -42,10 +42,27 @@ describe('AxiosHttpClient', () => {
         }
       })
       const httpResponse = await sut.post({ url, body })
+
       expect(httpResponse).toEqual({
         statusCode: 'any_status',
         body: 'any_data'
       })
+    })
+  })
+
+  describe('get()', () => {
+    beforeAll(() => {
+      mockedAxios.get.mockResolvedValue({ status: 200, data: 'any_data' })
+    })
+
+    beforeEach(() => {
+      sut = new AxiosHttpClient()
+    })
+
+    it('should call axios.get with correct values', async () => {
+      await sut.get({ url })
+
+      expect(mockedAxios.get).toHaveBeenCalledWith(url)
     })
   })
 })
