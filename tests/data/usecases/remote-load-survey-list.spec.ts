80ba0579993272f2b7cbfbc5ce32c3c9c0177c23
Leandro Guina <guina86@gmail.com>
2022-06-18 12:52:23 -0300

    test: ensure RemoteLoadSurveyList throws UnexpectedError on 403

--- tests/data/usecases/remote-load-survey-list.spec.ts
+++ tests/data/usecases/remote-load-survey-list.spec.ts
@@ -1,15 +1,18 @@
-import { HttpGetClient } from '@data/protocols'
+import { HttpGetClient, HttpStatusCode } from '@data/protocols'
 import { RemoteLoadSurveyList } from '@data/usecases'
+import { UnexpectedError } from '@domain/errors'
+import { SurveyModel } from '@domain/model'
 import { faker } from '@faker-js/faker'
 import { mock } from 'jest-mock-extended'
 
 describe('RemoteLoadSurveyList', () => {
   let sut: RemoteLoadSurveyList
   let url: string
-  const httpGetClientSpy = mock<HttpGetClient>()
+  const httpGetClientSpy = mock<HttpGetClient<SurveyModel[]>>()
 
   beforeAll(() => {
     url = faker.internet.url()
+    httpGetClientSpy.get.mockResolvedValue({ statusCode: HttpStatusCode.ok })
   })
 
   beforeEach(() => {
@@ -20,5 +23,12 @@ describe('RemoteLoadSurveyList', () => {
     await sut.loadAll()
 
     expect(httpGetClientSpy.get).toHaveBeenCalledWith({ url })
+  })
+
+  it('should throw UnexpectedError if HttpGetClient returns 403', async () => {
+    httpGetClientSpy.get.mockResolvedValueOnce({ statusCode: HttpStatusCode.forbidden })
+    const promise = sut.loadAll()
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
