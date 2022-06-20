f017cacfe4a314b2452e8feaf34ed13b9e9c4a12
Leandro Guina <guina86@gmail.com>
2022-06-20 12:38:16 -0300

    refactor: change PrivateRoute test format

--- tests/presentation/components/private-route.spec.tsx
+++ tests/presentation/components/private-route.spec.tsx
@@ -2,44 +2,40 @@ import React from 'react'
 import { Route, Router, Routes } from 'react-router-dom'
 import { render } from '@testing-library/react'
 import PrivateRoute from '@presentation/components/private-route'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import { SurveyList } from '@presentation/pages'
 import { ApiContext } from '@presentation/contexts'
+import { AccountModel } from '@domain/model'
 
 describe('PrivateRoute', () => {
-  const history = createMemoryHistory({ initialEntries: ['/'] })
+  let history: MemoryHistory
+  const prepare = (account: AccountModel | null): void => {
+    render(
+        <ApiContext.Provider value={{ getCurrentAccount: () => account }} >
+        <Router location={history.location} navigator={history}>
+          <Routes>
+            <Route element={<PrivateRoute />}>
+              <Route path= "/" element={<SurveyList />} />
+            </Route>
+          </Routes>
+        </Router>
+      </ApiContext.Provider>
+    )
+  }
+
+  beforeEach(() => {
+    history = createMemoryHistory({ initialEntries: ['/'] })
+  })
 
   it('should redirect to /login if token is empty', () => {
-    render(
-      <ApiContext.Provider value={{ getCurrentAccount: () => null }} >
-        <Router location={history.location} navigator={history}>
-          <Routes>
-            <Route path="/" element={<PrivateRoute />}>
-              <Route element={<SurveyList />} />
-            </Route>
-          </Routes>
-        </Router>
-      </ApiContext.Provider>
-    )
+    prepare(null)
+
     expect(history.location.pathname).toBe('/login')
   })
-})
-
-describe('PrivateRoute', () => {
-  const history = createMemoryHistory({ initialEntries: ['/'] })
 
   it('should render current component if token is not empty', () => {
-    render(
-      <ApiContext.Provider value={{ getCurrentAccount: () => ({ accessToken: 'any_token', name: 'any_name' }) }} >
-        <Router location={history.location} navigator={history}>
-          <Routes>
-            <Route path="/" element={<PrivateRoute />}>
-              <Route element={<SurveyList />} />
-            </Route>
-          </Routes>
-        </Router>
-      </ApiContext.Provider>
-    )
+    prepare({ accessToken: 'any_token', name: 'any_name' })
+
     expect(history.location.pathname).toBe('/')
   })
 })
