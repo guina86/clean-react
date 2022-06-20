27cec726a8a3ce9652bf2c8387091e4f203b8f80
Leandro Guina <guina86@gmail.com>
2022-06-20 17:52:07 -0300

    test: ensure SurveyList calls LoadSurveyList

--- tests/presentation/pages/survey-list.spec.tsx
+++ tests/presentation/pages/survey-list.spec.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { SurveyList } from '@presentation/pages'
+import mock from 'jest-mock-extended/lib/Mock'
+import { LoadSurveyList } from '@domain/usecases'
 
 describe('SurveyList', () => {
+  const loadSurveyListSpy = mock<LoadSurveyList>()
+
+  beforeAll(() => {
+    loadSurveyListSpy.loadAll.mockResolvedValue([])
+  })
+
   beforeEach(() => {
-    render(<SurveyList />)
+    jest.clearAllMocks()
+    render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
   })
 
   it('should present 4 empty items on start', () => {
     const surveyList = screen.getByRole('survey-list')
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+  })
+
+  it('should call LoadSurveyList', () => {
+    expect(loadSurveyListSpy.loadAll).toHaveBeenCalledTimes(1)
   })
 })
