7b38ae9cd9665da3dcb4745514d7efcaf6826134
Leandro Guina <guina86@gmail.com>
2022-06-20 07:41:38 -0300

    test: ensure LocalStorageAdapter calls localStorage.getItem with correct value

--- tests/infra/cache/local-storage-adapter.spec.ts
+++ tests/infra/cache/local-storage-adapter.spec.ts
@@ -3,16 +3,23 @@ import 'jest-localstorage-mock'
 
 describe('LocalStorageAdapter', () => {
   let sut: LocalStorageAdapter
+  const value = { any_key: 'any_value' }
 
   beforeEach(() => {
     localStorage.clear()
     sut = new LocalStorageAdapter()
   })
 
-  it('should call localStorage with correct values', async () => {
-    const value = { any_key: 'any_value' }
+  it('should call localStorage.setItem with correct values', async () => {
     sut.set('any_key', value)
 
     expect(localStorage.setItem).toHaveBeenCalledWith('any_key', JSON.stringify(value))
+  })
+
+  it('should call localStorage.getItem with correct value', async () => {
+    jest.spyOn(localStorage, 'getItem').mockReturnValueOnce(JSON.stringify(value))
+    const obj = sut.get('any_key')
+    expect(localStorage.getItem).toHaveBeenCalledWith('any_key')
+    expect(obj).toEqual(value)
   })
 })
