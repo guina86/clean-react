6879c8b3a4918a053716e2126b39c7cd1bb643bb
Leandro Guina <guina86@gmail.com>
2022-06-22 10:45:46 -0300

    test: ensure SurveyList render SurveyItems on success

--- tests/presentation/pages/survey-list.spec.tsx
+++ tests/presentation/pages/survey-list.spec.tsx
@@ -1,27 +1,38 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { SurveyList } from '@presentation/pages'
 import mock from 'jest-mock-extended/lib/Mock'
 import { LoadSurveyList } from '@domain/usecases'
+import { mockSurveyList } from '@tests/data/mocks/survey'
 
 describe('SurveyList', () => {
   const loadSurveyListSpy = mock<LoadSurveyList>()
 
   beforeAll(() => {
-    loadSurveyListSpy.loadAll.mockResolvedValue([])
+    loadSurveyListSpy.loadAll.mockResolvedValue(mockSurveyList())
   })
 
   beforeEach(() => {
     jest.clearAllMocks()
+  })
+
+  it('should present 4 empty items on start', async () => {
     render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
-  })
-
-  it('should present 4 empty items on start', () => {
     const surveyList = screen.getByRole('survey-list')
-    expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+    expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4)
+    await waitFor(() => surveyList)
   })
 
-  it('should call LoadSurveyList', () => {
+  it('should call LoadSurveyList', async () => {
+    render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
     expect(loadSurveyListSpy.loadAll).toHaveBeenCalledTimes(1)
+    await screen.findByRole('heading')
+  })
+
+  it('should render surveyItems ons success', async () => {
+    render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
+    await screen.findAllByRole('survey-item')
+    const surveyList = screen.getByRole('survey-list')
+    expect(surveyList.querySelectorAll('li.surveyItemWrap')).toHaveLength(4)
   })
 })
