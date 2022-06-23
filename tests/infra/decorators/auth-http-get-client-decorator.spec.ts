e7fcc9f09eac7a42473a596c36d9396d46c35989
Leandro Guina <guina86@gmail.com>
2022-06-23 16:25:19 -0300

    test: ensure AuthHttpGetClientDecorator not add token if GetStorage is invalid

--- tests/infra/decorators/auth-http-get-client-decorator.spec.ts
+++ tests/infra/decorators/auth-http-get-client-decorator.spec.ts
@@ -1,13 +1,24 @@
-import { GetStorage } from '@data/protocols'
+import { GetStorage, HttpGetClient } from '@data/protocols'
 import { AuthHttpGetClientDecorator } from '@main/decorators/auth-http-get-client-decorator'
 import { mock } from 'jest-mock-extended'
 
 describe('AuthHttpGetClientDecorator', () => {
+  let sut: AuthHttpGetClientDecorator
   const getStorageSpy = mock<GetStorage>()
+  const httpGetClientSpy = mock<HttpGetClient>()
+
+  beforeEach(() => {
+    sut = new AuthHttpGetClientDecorator(getStorageSpy, httpGetClientSpy)
+  })
 
   it('should call GetStorage with correct value', async () => {
-    const sut = new AuthHttpGetClientDecorator(getStorageSpy)
     await sut.get({ url: 'any_url', headers: { any: 'any' } })
     expect(getStorageSpy.get).toHaveBeenCalledWith('account')
+  })
+
+  it('should not add headers if getStorage is invalid', async () => {
+    const request = { url: 'any_url', headers: { any: 'any' } }
+    await sut.get(request)
+    expect(httpGetClientSpy.get).toHaveBeenCalledWith({ url: request.url, headers: { any: 'any' } })
   })
 })
