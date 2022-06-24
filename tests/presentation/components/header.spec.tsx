af47ec1237bcb0eb9ab2a202bd224e8427ad239b
Leandro Guina <guina86@gmail.com>
2022-06-24 16:42:20 -0300

    refactor: move sut creationg to a method

--- tests/presentation/components/header.spec.tsx
+++ tests/presentation/components/header.spec.tsx
@@ -2,14 +2,13 @@ import { fireEvent, render, screen } from '@testing-library/react'
 import React from 'react'
 import Header from '@presentation/components/header'
 import { ApiContext } from '@presentation/contexts'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import { Router } from 'react-router-dom'
 
 describe('Header', () => {
-  it('should call setCurrentAccount with null', () => {
-    const setCurrentAccountMock = jest.fn()
-    const history = createMemoryHistory({ initialEntries: ['/'] })
-
+  let history: MemoryHistory
+  const setCurrentAccountMock = jest.fn()
+  const renderSut = (): void => {
     render(
       <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock }}>
         <Router location={history.location} navigator={history}>
@@ -17,9 +16,16 @@ describe('Header', () => {
         </Router>
       </ApiContext.Provider>
     )
+  }
 
+  beforeEach(() => {
+    history = createMemoryHistory({ initialEntries: ['/'] })
+    jest.clearAllMocks()
+  })
+
+  it('should call setCurrentAccount with null', () => {
+    renderSut()
     fireEvent.click(screen.getByRole('logout'))
-
     expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined)
     expect(setCurrentAccountMock).toHaveBeenCalledTimes(1)
     expect(history.location.pathname).toBe('/login')
