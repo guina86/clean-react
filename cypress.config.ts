c1cd105fabea54233f753e390e04fa31c7501026
Leandro Guina <guina86@gmail.com>
2022-06-25 20:18:22 -0300

    test: ensure SurveyList reload on button click

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080',
-    fixturesFolder: 'cypress/fixtures',
     supportFile: 'cypress/support/commands.js',
     setupNodeEvents (on, config) {
 
--- cypress/e2e/survey-list.cy.ts
+++ cypress/e2e/survey-list.cy.ts
@@ -1,8 +1,10 @@
 import { getLocalStorageItem, setLocalStorageItem, testUrl } from '../utils/helpers'
-import { mockApiError } from '../utils/http-mock'
+import { mockApiError, mockApiSuccess } from '../utils/http-mock'
 
 describe('SurveyList', () => {
-  beforeEach(() => {
+  let list: any
+  beforeEach(function () {
+    cy.fixture('survey-list').then((data) => { list = data })
     setLocalStorageItem('account', { accessToken: 'any_token', name: 'any_name' })
   })
 
@@ -10,6 +12,15 @@ describe('SurveyList', () => {
     mockApiError(/surveys/, 'GET', 400)
     cy.visit('')
     cy.getByRole('error-message').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+  })
+
+  it('should reload on button click', () => {
+    mockApiError(/surveys/, 'GET', 400)
+    cy.visit('')
+    cy.getByRole('error-message').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+    mockApiSuccess(/surveys/, 'GET', list)
+    cy.get('button').click()
+    cy.getByRole('survey-item').should('have.length', 2)
   })
 
   it('should logout on AccessDeniedError', () => {
@@ -30,5 +41,30 @@ describe('SurveyList', () => {
     cy.visit('')
     cy.getByRole('logout').click()
     testUrl('/login')
+  })
+
+  it('should present survey items', () => {
+    mockApiSuccess(/surveys/, 'GET', list)
+    cy.visit('')
+    cy.getByRole('empty-item').should('exist')
+    cy.getByRole('survey-item').should('have.length', 2)
+    cy.get('li:nth-child(1)').then(li => {
+      assert.equal(li.find('[role="date-day"]').text(), '03')
+      assert.equal(li.find('[role="date-month"]').text(), 'fev')
+      assert.equal(li.find('[role="date-year"]').text(), '2021')
+      assert.equal(li.find('[role="question-text"]').text(), 'Question 1')
+      cy.fixture('icons').then(icon => {
+        assert.equal(li.find('[role="image-icon"]').attr('src'), icon.thumbUp)
+      })
+    })
+    cy.get('li:nth-child(2)').then(li => {
+      assert.equal(li.find('[role="date-day"]').text(), '04')
+      assert.equal(li.find('[role="date-month"]').text(), 'mar')
+      assert.equal(li.find('[role="date-year"]').text(), '2022')
+      assert.equal(li.find('[role="question-text"]').text(), 'Question 2')
+      cy.fixture('icons').then(icon => {
+        assert.equal(li.find('[role="image-icon"]').attr('src'), icon.thumbDown)
+      })
+    })
   })
 })
