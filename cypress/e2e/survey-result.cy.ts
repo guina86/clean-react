d30dffcf234c81389847c585a4dba6244ef3ea93
Leandro Guina <guina86@gmail.com>
2022-06-28 16:22:29 -0300

    test: ensure SurveyResult present correct data on success

--- cypress/e2e/survey-result.cy.ts
+++ cypress/e2e/survey-result.cy.ts
@@ -28,4 +28,24 @@ describe('SurveyResult', () => {
     cy.visit('/surveys/any_id')
     testUrl('/login')
   })
+
+  it('should present survey result', () => {
+    mockApiSuccess(/api\/surveys/, 'GET', surveyResult)
+    cy.visit('/surveys/any_id')
+    cy.getByRole('question').should('have.text', 'any_question')
+    cy.getByRole('date-day').should('have.text', '03')
+    cy.getByRole('date-month').should('have.text', 'fev')
+    cy.getByRole('date-year').should('have.text', '2021')
+    cy.get('li:nth-child(1)').then(li => {
+      assert.equal(li.find('[role="answer"]').text(), 'any_answer')
+      assert.equal(li.find('[role="answer-percent"]').text(), '75%')
+      assert.equal(li.find('[role="answer-image"]').attr('src'), 'any_image')
+      assert.equal(li.css('box-shadow'), 'rgb(188, 71, 123) 0px 0px 3px 2px')
+    })
+    cy.get('li:nth-child(2)').then(li => {
+      assert.equal(li.find('[role="answer"]').text(), 'any_answer_2')
+      assert.equal(li.find('[role="answer-percent"]').text(), '25%')
+      assert.notExists(li.find('[role="answer-image"]'))
+    })
+  })
 })
