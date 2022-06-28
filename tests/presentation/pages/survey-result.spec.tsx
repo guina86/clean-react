7e17272a15319fb4b2644df7e7a7ace651526c81
Leandro Guina <guina86@gmail.com>
2022-06-27 22:32:05 -0300

    test: ensure SurveyResult present correct initial state

--- tests/presentation/pages/survey-result.spec.tsx
+++ tests/presentation/pages/survey-result.spec.tsx
@@ -1,4 +1,27 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { SurveyResult } from '@presentation/pages'
+import { ApiContext } from '@presentation/contexts'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+
 describe('SurveyResult', () => {
-  it('should ', () => {
+  const history = createMemoryHistory({ initialEntries: ['/'] })
+  const renderSut = (): void => {
+    render(
+      <ApiContext.Provider value={{ getCurrentAccount: jest.fn(), setCurrentAccount: jest.fn() }}>
+        <Router location={history.location} navigator={history}>
+          <SurveyResult />
+        </Router>
+      </ApiContext.Provider>
+    )
+  }
+
+  it('should present correct initial state', async () => {
+    renderSut()
+    const surveyContainer = screen.getByRole('survey-container')
+    expect(surveyContainer.childElementCount).toBe(0)
+    expect(screen.queryByRole('loading')).not.toBeInTheDocument()
+    expect(screen.queryByRole('error-message')).not.toBeInTheDocument()
   })
 })
