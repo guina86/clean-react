ae0df2b2c21426bffeea5568262d9dd11edf99f6
Leandro Guina <guina86@gmail.com>
2022-06-27 22:01:03 -0300

    fix: move Calendar tests back to SurveyList

--- tests/presentation/pages/components/survey-item.spec.tsx
+++ tests/presentation/pages/components/survey-item.spec.tsx
@@ -6,20 +6,26 @@ import { IconName } from '@presentation/components'
 import { LoadSurveyList } from '@domain/usecases'
 
 describe('SurveyItem', () => {
-  const prepare = (didAnswer: boolean): LoadSurveyList.Model => {
-    const survey = mockSurvey(didAnswer)
+  const renderSut = (didAnswer: boolean, date: Date): LoadSurveyList.Model => {
+    const survey = mockSurvey(didAnswer, date)
     render(<SurveyItem survey={survey} />)
     return survey
   }
 
   it('should render with correct values', () => {
-    const survey = prepare(true)
+    const survey = renderSut(true, new Date('2022-01-10T00:00:00'))
+    expect(screen.getByRole('date-day')).toHaveTextContent('10')
+    expect(screen.getByRole('date-month')).toHaveTextContent('jan')
+    expect(screen.getByRole('date-year')).toHaveTextContent('2022')
     expect(screen.getByRole('image-icon')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByRole('question-text')).toHaveTextContent(survey.question)
   })
 
   it('should render with correct values', () => {
-    const survey = prepare(false)
+    const survey = renderSut(false, new Date('2020-10-02T00:00:00'))
+    expect(screen.getByRole('date-day')).toHaveTextContent('02')
+    expect(screen.getByRole('date-month')).toHaveTextContent('out')
+    expect(screen.getByRole('date-year')).toHaveTextContent('2020')
     expect(screen.getByRole('image-icon')).toHaveProperty('src', IconName.thumbDown)
     expect(screen.getByRole('question-text')).toHaveTextContent(survey.question)
   })
